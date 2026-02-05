e5729b586241c8380733fb48777379417f5d8f0d
Coniferous <adoria247@gmail.com>
2026-02-04 22:10:23 -0500

    Contact page basics

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,42 @@
 'use client';
-import Image from "next/image";
 import { useState } from 'react';
 import "./Contact.css";
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   return (
-    <div>
-      Contact
+    <div className="contact-container">
+      <div className="contact-form">
+        <div className="name-section">
+          <input 
+            type="text"
+            placeholder="Name"
+            className="name-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="email-section">
+          <input 
+            type="email"
+            placeholder="Email"
+            className="email-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="message-section">
+          <textarea 
+            placeholder="Message"
+            className="message-input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }

c2c2ca07337963e55103eb65b8363bbede15348d
Coniferous <adoria247@gmail.com>
2026-02-05 21:31:38 -0500

    Logos

--- app/page.tsx
+++ app/page.tsx
@@ -19,168 +19,168 @@ export default function Home() {
       id: 1, 
       name: 'HTML', 
       categories: ["generic", "ui/ux", "webdev"],
-      logo: '/logos/HTML5_logo_and_wordmark.svg.png'
+      logo: 'logos/html-5-svgrepo-com.svg'
     },
     { 
       id: 2, 
       name: 'JavaScript', 
       categories: ["generic", "OOP", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/javascript-svgrepo-com.svg"
     },
     { 
       id: 3, 
       name: 'CSS', 
       categories: ["generic", "ui/ux", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/css-3-svgrepo-com.svg"
     },
     { 
       id: 4, 
       name: 'React', 
       categories: ["generic", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/react-svgrepo-com.svg"
     },
     { 
       id: 5, 
       name: 'Next.js', 
       categories: ["generic", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/nextjs-fill-svgrepo-com.svg"
     },
     { 
       id: 6, 
       name: 'git', 
       categories: ["generic", "webdev", ],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/git-svgrepo-com.svg"
     },
     { 
       id: 7, 
       name: 'node.js', 
       categories: ["generic", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/nodejs-icon-svgrepo-com.svg"
     },
     { 
       id: 8, 
       name: 'Python', 
       categories: ["generic", "OOP", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/python-svgrepo-com.svg"
     },
     { 
       id: 9, 
       name: 'MySQL', 
       categories: ["generic", "db"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/mysql-logo-svgrepo-com.svg"
     },
     { 
       id: 10, 
       name: 'PostgreSQL', 
       categories: ["generic", "db"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/postgresql-svgrepo-com.svg"
     },
     { 
       id: 11, 
       name: 'Bootstrap Studio', 
       categories: ["ui/ux", "webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/bootstrap-svgrepo-com.svg"
     },
     { 
       id: 12, 
       name: 'Rest APIs', 
       categories: ["webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/rest-api-svgrepo-com.svg"
     },
     { 
       id: 13, 
       name: 'Machine Learning', 
       categories: ["ai"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/machine-learning-model-svgrepo-com.svg"
     },
     { 
       id: 14, 
       name: 'Prompt Engineering', 
       categories: ["ai"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/ai-svgrepo-com.svg"
     },
     { 
       id: 15, 
       name: 'OpenAI API', 
       categories: ["ai"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/openai-svgrepo-com.svg"
     },
     { 
       id: 16, 
       name: 'YOLO Classification', 
       categories: ["ai"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/classifier-language-svgrepo-com.svg"
     },
     { 
       id: 17, 
       name: 'Svelte', 
       categories: ["webdev"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/svelte-icon-svgrepo-com.svg"
     },
     { 
       id: 18, 
       name: 'Clickup', 
       categories: ["team"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/clickup-svgrepo-com.svg"
     },
     { 
       id: 19, 
       name: 'Slack', 
       categories: ["team"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/slack-svgrepo-com.svg"
     },
     { 
       id: 20, 
       name: 'Java', 
       categories: ["OOP"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/java-svgrepo-com.svg"
     },
     { 
       id: 21, 
       name: 'C#', 
       categories: ["OOP"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/c-sharp-svgrepo-com.svg"
     },
     { 
       id: 22, 
       name: 'IOT Programming', 
       categories: ["iot"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/iot-core-svgrepo-com.svg"
     },
     { 
       id: 23, 
       name: 'Arduino Framework', 
       categories: ["iot"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/arduino-svgrepo-com.svg"
     },
     { 
       id: 24, 
       name: 'Teamwork', 
       categories: ["transferrable"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/teamwork-svgrepo-com.svg"
     },
     { 
       id: 25, 
       name: 'Customer Service', 
       categories: ["transferrable"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/customer-service-support-svgrepo-com.svg"
     },
     { 
       id: 26, 
-      name: 'Customer Relationship Management (CRM)', 
+      name: 'Customer Relationship Management', 
       categories: ["transferrable"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/zoho-svgrepo-com.svg"
     },
     { 
       id: 27, 
       name: 'Time Management', 
       categories: ["transferrable"],
-      logo: "https//:www.logo.com" //TODO
+      logo: "logos/time-svgrepo-com.svg"
     },
     { id: 28, 
       name: 'Figma', 
       categories: ["ui/ux"], 
-      logo: "https//:www.logo.com" 
+      logo: "logos/figma-svgrepo-com.svg" 
     },
 
   ];
